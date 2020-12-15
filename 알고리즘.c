e76c3d0f096264e5ca4f1694b6ca132e93246f0f
inwooshin <60161818@mju.ac.kr>
2020-12-15 12:24:52 +0900

    1214 알고리즘 수정

--- 알고리즘.c
+++ 알고리즘.c
@@ -5,26 +5,15 @@
 // numbers_len은 배열 numbers의 길이입니다.
 int* solution(int numbers[], size_t numbers_len) {
 	// return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-	int* answer = (int*)malloc(numbers_len * sizeof(int));
-	int compare, sum, tmp, isOut = 0, min = 100, minpos;
+	int* answer = (int*)malloc(numbers_len * numbers_len * sizeof(int));
+	int compare, sum, tmp, isOut = 0;
 	int resultIn = 0;
-
-	for (int i = 0; i < numbers_len; i++) {
-		for (int j = i + 1; j < numbers_len; j++) {
-			if (numbers[i] > numbers[j]) {
-				tmp = numbers[i];
-				numbers[i] = numbers[j];
-				numbers[j] = tmp;
-			}
-
-		}
-	}
 
 	for (int i = 0; i < numbers_len; i++) {
 		compare = numbers[i];
 		for (int j = i + 1; j < numbers_len; j++) {
 			sum = numbers[j] + compare;
-			for (int k = 0; k <= resultIn; k++) {
+			for (int k = 0; k < resultIn; k++) {
 				if (answer[k] == sum) {
 					isOut = 1;
 					break;
@@ -39,6 +28,17 @@ int* solution(int numbers[], size_t numbers_len) {
 
 		}
 
+	}
+
+	for (int i = 0; i < resultIn; i++) {
+		for (int j = i + 1; j < resultIn; j++) {
+			if (answer[i] > answer[j]) {
+				tmp = answer[i];
+				answer[i] = answer[j];
+				answer[j] = tmp;
+			}
+
+		}
 	}
 
 	return answer;
