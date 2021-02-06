cb123963523141f2d610b1d9c8392fbf760f8dac
inwooshin <60161818@mju.ac.kr>
2021-02-06 13:07:17 +0900

    타겟 넘버 모든 경우의 수를 따지는 법
    모든 경우의 수를 따지기 위해서는 2^n 승 개일 때 dfs로 재귀를 사용하는 방법이 있다.
    이 방법은 실행면에서 빠른 속도를 보여준다.
    
    그리고 비트 연산을 해서 따지는 방법이 있다.
    
    이 두가지 방법은 앞으로도 많이 쓰일 것 같다.

--- C++/타겟넘버.cpp
+++ C++/타겟넘버.cpp
@@ -4,37 +4,65 @@
 
 using namespace std;
 
+// 모든 경우의 수를 비트 연산자를 사용해서 계산한 것...! 
 int solution(vector<int> numbers, int target) {
-    int answer = 0, before = 0, count = 0;
-    vector<bool> check(numbers.size(), 0);
-    for(int i = 0 ; i < numbers.size(); i++) before += numbers[i];
-    int all = before;
+    int answer = 0, all = 0, before = 0;
+    int size = 1 << numbers.size();
     
-    do{
-        for(int i = 0 ; i < numbers.size(); i++){
-            if(all < target) break;
-            else if(all == target){answer++;
-                                  continue;}
-            all -= numbers[i] * 2;
+    for(int i = 0 ; i < numbers.size() ; i++){
+        before += numbers[i];
+    }
+    
+    for(int i = 0 ; i < size ; i++){
+        all = before;
+        for(int j = 0 ; j < numbers.size(); j++){
+            if(i & (1 << j)){
+                all -= numbers[j] * 2;
+            }
         }
-    }while(next_permutation(numbers.begin(), numbers.end()));
+        if(all == target) answer++;
+    }
     
     return answer;
 }
 
 // 참고용 코드 DFS 를 사용해서 값을 도출한  것 
-void dfs(vector<int>& numbers, int& answer, int target, int count = 0, int sum = 0){
-    if (count == numbers.size() - 1) {
-        if (target == sum + numbers[count])        answer++;
-        if (target == sum - numbers[count])        answer++;
+void dfs(vector<int>& numbers, int target, int& answer, int count, int all){
+    if(count == numbers.size() - 1){
+        if(all + numbers[count] == target) answer++;
+        if(all - numbers[count] == target) answer++;
+        
         return;
     }
-    dfs(numbers, answer, target, count + 1, sum + numbers[count]);
-    dfs(numbers, answer, target, count + 1, sum - numbers[count]);
+    
+    dfs(numbers, target, answer, count + 1, all + numbers[count]);
+    dfs(numbers, target, answer, count + 1, all - numbers[count]);
 }
- 
+
 int solution(vector<int> numbers, int target) {
     int answer = 0;
-    dfs(numbers, answer, target);
+    dfs(numbers, target, answer, 0, 0);
     return answer;
 }
+
+// 비트 연산을 한 것! 
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    int size = 1 << numbers.size();
+
+    for(int i = 1 ; i < size ; i++){
+        int temp = 0;
+        for(int j = 0 ; j < numbers.size() ; j++)
+        {  
+            if(i &(1 << j)){
+                temp -= numbers[j];
+            }
+            else temp += numbers[j];
+        }
+        if(temp == target) answer++;
+    }
+    return answer;
+}
+
+
+
