63f7a3933951e50649504a5dbae7840db9aad6ea
inwooshin <60161818@mju.ac.kr>
2021-02-08 22:32:17 +0900

    문자열 합축 문제 풀이 성공
    1. 찾고자 하는 패턴이 이전과 같을 때 중복 횟수를 증가시킨다.
     그러나 찾고자 하는 패턴이 이전과 다를 경우에 두가지로 나뉜다.
    1-1. count 가 0이 아닐 경우에는 이전의 횟수와 이전의 문자를 zip에 더해준다.
    1-2. count 가 0일 경우 이전의 문자를 zip에 더해준다.
    
    2. 그리고 찾고자하는 문자열의 길이가 남은 문자열의 길이와 같을 경우와 클 경우로 나뉜다
    2-1. 길이가 같다면 그 찾고자 하는 문자열이 이전의 패턴과 같은지 확인하고 패턴과 같으면 문자열을 카운트와 패턴을 zip에 넣어준다.
    2-2. 길이가 같지만 패턴이 같지 않거나, 길이가 크다면 다시 두가지로 나뉜다.
    2-2-1. 카운트가 있을 경우 카운트와 이전의 문자열을 넣어준다. 그리고 나머지 문자열도 넣어준다.
    2-2-2. 카운트가 없을 경우 이전의 패턴과 나머지 문자열을 넣어준다.

--- C++/문자열압축.cpp
+++ C++/문자열압축.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 
+
 int solution(string s) {
-    int answer = s.length(), length = 1, count = 0, before = 1001, lenCount = 1;
+    int answer = s.length(), length = 1, count = 0, before = 1001;
     
     if(s.length() == 1) return 1;
     
@@ -15,38 +16,28 @@ int solution(string s) {
         string zip = "";
         for(int i = 0 ; i < s.length() ; i += length){
             string all = s.substr(i, s.length());
-            if(i+length >= s.length() - 1){
-                if(length <= s.length() - i && !com.compare(s.substr(i, length))) count++;
-                if(count > 0){
-                    zip += to_string(count + 1) + com;
-                                  count = 0;}
-                else if(count == 0) zip += com + s.substr(i, s.length());
-                
+            if(i+length >= s.length()){
+                if(length == s.length() - i && !com.compare(s.substr(i, length))){
+                    zip += to_string(count + 2) + com;
+                }
+                else if(count > 0){
+                    zip += to_string(count + 1) + com + s.substr(i, s.length());}
+                else zip += com + s.substr(i, s.length());
                 break;
             }
-            if(all.find(s.substr(i, length)) == 0){
-                if(!com.compare(s.substr(i, length))){count++;}
-                else if(count > 0){
-                    zip += to_string(count + 1) + com;
-                    count = 0;
-                }
-                else if(i != 0 && count == 0){zip += s.substr(i, length);}
-
-                com = s.substr(i, length);
+            if(!com.compare(s.substr(i, length))){count++;}
+            else if(count > 0){
+                zip += to_string(count + 1) + com;
+                count = 0;
             }
-            else{zip += s.substr(i, length);}
-            
+            else if(i != 0 && count == 0){zip += com;}
+            com = s.substr(i, length);
         }
-        cout << "length : " << length << ", zip : " << zip << endl;
-        if(zip.empty()){
-            count = 0;
-            length++;
-            continue;}
         if(before > zip.length()) before = zip.length();
         count = 0;
         length++;
-        
     }
     
     return before;
 }
+
