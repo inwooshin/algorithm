02e67d341f391c8d1bdb7677a71345b1fd589aca
inwooshin <60161818@mju.ac.kr>
2021-02-10 20:02:44 +0900

    컬러링 븍 수정
    만약 비교를 해서 해당 값을 변경을 할 경우에는 그 값에서 재귀를 사용해서 다시 비교를 하는 것으로 전부 같은 영역으로 인식할 수 있도록 하여주었다. 그러나 값이 결과에서 인정되지 않았다.
    답을 확인하고 문제의 오류를 찾거나, 코드의 오류를 찾는 식으로 해결해보아야겠다. 또한 반복적인 코드를 호출하는 setArea 함수에서 많이 사용해서 이를 반복문으로 짧게 변경해보아야겠다.

--- C++/카카오프렌즈 컬러링북.cpp
+++ C++/카카오프렌즈 컬러링북.cpp
@@ -4,104 +4,63 @@
 
 using namespace std;
 
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
-vector<int> solution(int m, int n, vector<vector<int>> p) {
-    vector<vector<long>> picture(m, vector<long>(n, 0));
-    for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                picture[i][j]=(long)p[i][j];
-            }
-    }
-    int max = 0, all, once = 0, near = 0, reCount = 0;
-    vector<vector<int>> table(m, vector<int>(n, 0));
-    vector<int> count(m*n, 0);
-    vector<vector<long>> overlap(m * n, vector<long>(1, 0));
-    vector<long> check(m * n, 0);
-    map<long, long> reIndex;
-    
-    vector<int> answer(2);
-    answer[0] = 0;
-    answer[1] = 0;
-    for(int i = 0 ; i < m ; i++){
-        for(int j = 0 ; j < n ; j++){
-            near = 0;
-            if(!once && picture[i][j]){table[i][j] = ++answer[0];   once = 1;  }
-            if(picture[i][j] == 0) continue;
+vector<int> count;
+vector<vector<long>> overlap;
+vector<long> check;
+map<long, long> reIndex;
+int once = 0, near = 0, reCount = 0, countAll = 0;
+
+void setArea(int m, int n, int i, int j, vector<vector<int>> picture, vector<vector<int>>& table, vector<int>& answer){
+    near = 0;
+    if(!once && picture[i][j]){table[i][j] = ++answer[0];   once = 1;}
+            if(picture[i][j] == 0) return;
             if(i-1 >= 0 && picture[i][j] == picture[i-1][j]){
                 near++;
                 if(table[i][j] != 0 && table[i-1][j] != 0 && table[i-1][j] != table[i][j]){
                     if(table[i][j] > table[i-1][j]){
-                        if(!check[table[i][j]]){
-                            overlap[table[i-1][j]].push_back(table[i][j]);
-                            check[table[i][j]] = 1;
-                            reCount++;
-                        }
                         table[i][j] = table[i-1][j];
+                        setArea(m, n, i, j, picture, table, answer);
                     }
                     else if(table[i][j] < table[i-1][j]){
-                        if(!check[table[i-1][j]]){
-                           overlap[table[i][j]].push_back(table[i-1][j]);
-                           check[table[i-1][j]] = 1;
-                            reCount++;
-                        }
                         table[i-1][j] = table[i][j];
+                        setArea(m, n,i-1, j, picture, table, answer);
                     }
                 }
                 if(table[i][j] == 0 && table[i-1][j] != 0) {
                     table[i][j] = table[i-1][j];
                                        }
                 else if(table[i][j] == 0 && table[i-1][j] == 0){
-                    answer[0]++;
                     table[i][j] = answer[0];
-                    table[i][j+1] = answer[0];
                 }
             }
             if(j-1 >= 0 && picture[i][j] == picture[i][j-1]){
                 near++;
                 if(table[i][j] != 0 && table[i][j-1] != 0 && table[i][j-1] != table[i][j]){
                     if(table[i][j] > table[i][j-1]){
-                        if(!check[table[i][j]]){
-                            overlap[table[i][j-1]].push_back(table[i][j]);
-                            check[table[i][j]] = 1;
-                            reCount++;
-                        }
                         table[i][j] = table[i][j-1];
+                        setArea(m, n,i, j, picture, table, answer);
                     }
                     else if(table[i][j] < table[i][j-1]){
-                        if(!check[table[i][j-1]]){
-                           overlap[table[i][j]].push_back(table[i][j-1]);
-                           check[table[i][j-1]] = 1;
-                            reCount++;
-                        }
                         table[i][j-1] = table[i][j];
+                        setArea(m, n,i, j-1, picture, table, answer);
                     }
                 }
                 
                 if(table[i][j] == 0 && table[i][j-1] != 0) table[i][j] = table[i][j-1];
                 else if(table[i][j] == 0 && table[i][j-1] == 0){
-                    answer[0]++;
                     table[i][j] = answer[0];
-                    table[i][j+1] = answer[0];
                 }
             }
             if(i+1 < m && picture[i][j] == picture[i+1][j]){
                 near++;
                 if(table[i][j] != 0 && table[i+1][j] != 0 && table[i+1][j] != table[i][j]){
                     if(table[i][j] > table[i+1][j]){
-                        if(!check[table[i][j]]){
-                            overlap[table[i+1][j]].push_back(table[i][j]);
-                            check[table[i][j]] = 1;
-                            reCount++;
-                        }
                         table[i][j] = table[i+1][j];
+                        setArea(m, n,i, j, picture, table, answer);
                     }
                     else if(table[i][j] < table[i+1][j]){
-                        if(!check[table[i+1][j]]){
-                           overlap[table[i][j]].push_back(table[i+1][j]);
-                           check[table[i+1][j]] = 1;
-                            reCount++;
-                        }
                         table[i+1][j] = table[i][j];
+                        setArea(m, n,i+1, j, picture, table, answer);
                     }
                 }
                 
@@ -109,27 +68,18 @@ vector<int> solution(int m, int n, vector<vector<int>> p) {
                 else if(table[i][j] == 0 && table[i+1][j] == 0){
                     answer[0]++;
                     table[i][j] = answer[0];
-                    table[i][j+1] = answer[0];
                 }
             }
             if(j+1 < n && picture[i][j] == picture[i][j + 1]){
                 near++;
                 if(table[i][j] != 0 && table[i][j+1] != 0 && table[i][j+1] != table[i][j]){
                     if(table[i][j] > table[i][j+1]){
-                        if(!check[table[i][j]]){
-                            overlap[table[i][j+1]].push_back(table[i][j]);
-                            check[table[i][j]] = 1;
-                            reCount++;
-                        }
                         table[i][j] = table[i][j+1];
+                        setArea(m, n,i, j, picture, table, answer);
                     }
                     else if(table[i][j] < table[i][j+1]){
-                        if(!check[table[i][j+1]]){
-                           overlap[table[i][j]].push_back(table[i][j+1]);
-                           check[table[i][j+1]] = 1;
-                            reCount++;
-                        }
                         table[i][j+1] = table[i][j];
+                        setArea(m, n,i, j+1, picture, table, answer);
                     }
                 }
                 
@@ -137,36 +87,51 @@ vector<int> solution(int m, int n, vector<vector<int>> p) {
                 else if(table[i][j] == 0){
                     answer[0]++;
                     table[i][j] = answer[0];
-                    table[i][j+1] = answer[0];
                 }
             }
             if(near == 0){
                 answer[0]++;
                 table[i][j] = answer[0];
             }
+}
+
+vector<int> solution(int m, int n, vector<vector<int>> picture) {
+    int max = 0, all;
+    vector<vector<int>> table(m, vector<int>(n, 0));
+    count.assign(m*n, 0);
+    overlap.assign(m * n, vector<long>(1, 0));
+    check.assign(m * n, 0);
+    
+    vector<int> answer(2);
+    answer[0] = 0;
+    answer[1] = 0;
+    for(int i = 0 ; i < m ; i++){
+        for(int j = 0 ; j < n ; j++){
+            setArea(m,n,i, j, picture, table, answer);
         }
     }
     
     for(int i = 0 ; i < m ; i++){
         for(int j = 0 ; j < n ; j++){
-            count[table[i][j]]++;
-        }
-    }
-    
-    for(int i = 1 ; count[i] != 0 ; i++){
-        int nCount = 0;
-        if(overlap[i].back()){
-            for(int j = 1 ; j < overlap[i].size() ; j++){
-                nCount += count[overlap[i][j]];
-                //cout << "i : " << i << ", j : " << j << ", overlap : " << overlap[i][j] << endl;
+            if(table[i][j] != 0 && !check[table[i][j]]){
+                countAll++; 
+                check[table[i][j]] = 1;
             }
+            count[table[i][j]]++;
+            cout << table[i][j] << "    ";
         }
-        nCount += count[i];
-        //cout << "Count[" << i << "] : " << count[i] << endl;
-        if(max < nCount){max = nCount;}
+        cout << endl;
     }
     
-    answer[0] = answer[0] - reCount;
+    for(int i = 1 ; i < count.size() ; i++){
+        int nCount = 0;
+        if(count[i] == 0) continue;
+        //cout << "Count[" << i << "] : " << count[i] << endl;
+        if(max < count[i]){max = count[i];}
+    }
+    
+    
+    answer[0] = countAll;
     answer[1] = max;
     
     return answer;
