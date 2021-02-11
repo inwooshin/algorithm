179cb034e6915d229eb01993d5ea90532f083698
inwooshin <60161818@mju.ac.kr>
2021-02-11 16:41:32 +0900

    메뉴 리뉴얼
    문제 해설
    https://programmers.co.kr/learn/courses/30/lessons/72411
    
    해결
    조합을 사용해서 여러가지 문자열의 course 의 인덱스 별 갯수로 조합을 한다. 그리고 그 조합을 하고 난 후 애초에 원래의 문자열이 정렬되어 있지 않기 때문에, 제한 조건인 오름차순 정렬을 해준 뒤에 문자열을 키 값으로 map 에 삽입한다. 이 과정을 각 문자열 당 course 의 갯수만큼 course의 각 인덱스와 비교해서 반복해서 조합한다.
    
    그리고 map 에 각 문자별로 count 된 숫자를 가지고 course 의 인덱스 별 갯수에 해당하는 최대값을 구한다. 구하면서 같은 길이에 해당하는 문자열을 새로운 map 배열에 count 와 함께 넣어준다. 그래서 max 값과 같은 값을 가지는 문자열이 있는지 확인하고 있으면 정답에 넣어준다.
    
    마지막으로 최종 메뉴의 조합을 오름차순으로 넣어주어야하기 때문에 오름차순으로 정렬해준다.

---
+++ C++/메뉴리뉴얼.cpp
@@ -0,0 +1,84 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+map<string, int> match; 
+
+void combination(int n, int count, int j, vector<int>& check, string orders){
+    string ins = "";
+    vector<char> re;
+    if(count == n){
+        for(int i = 0 ; i < orders.size() ; i++)
+            if(check[i]){
+                re.push_back(orders[i]);
+            }
+        sort(re.begin(), re.end());
+        for(int i = 0 ; i < re.size() ; i++)
+            ins += re[i];
+    }
+    
+    if(count == n){
+        if(match.find(ins) == match.end()) match[ins] = 1;
+        else match[ins]++;
+        
+        return;
+    }
+    
+    for(int i = j; i < orders.size(); i++){
+        if(!check[i]){
+            check[i] = 1;
+            combination(n, count + 1, i+1, check, orders);
+            check[i] = 0;
+        }
+    }
+    
+}
+
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answer;
+    vector<char> compare;
+    vector<int> check(10,0);
+    int max = 0;
+    string all = "";
+    
+    for(int i = 0 ; i < orders.size() ; i++){
+        for(int j = 0 ; j < course.size() ; j++){
+            if(orders[i].length() >= course[j]){
+                combination(course[j], 0, 0, check, orders[i]);
+                check.clear();
+                check.resize(10, 0);
+            }
+        }
+        compare.clear();
+    }
+    
+    for(int i = 0 ; i < course.size() ; i++){
+        auto maxit = match.begin();
+        max = 0;
+        map<string, int> mCount;
+        for(auto it = match.begin() ; it != match.end() ; it++){
+            if(it->first.length() == course[i]){
+                mCount[it->first] = it->second;
+                if(max < it->second){max = it->second;    maxit = it;
+                }
+            }
+        }
+        if(max < 2) continue;
+        
+        for(auto it = mCount.begin() ; it != mCount.end() ; it++){
+            if(it->first.length() == course[i]){
+                if(max == it->second){
+                    answer.push_back(it->first);
+                }
+            }
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+
+    return answer;
+}
