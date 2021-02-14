2885ca5c647f88894c6d1e5ee6f72038200a77f9
inwooshin <60161818@mju.ac.kr>
2021-02-14 11:15:49 +0900

    카카오프렌즈 컬러링북
    어피치의 모습의 예제를 통해서 visual studio 에서 예제를 통해 값이 잘 나오는지 확인해 보았다.

--- C++/카카오프렌즈 컬러링북.cpp
+++ C++/카카오프렌즈 컬러링북.cpp
@@ -1,8 +1,9 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-vector<vector<int>> check;
+vector<vector<int> > check;
 
 int x[4] = {0,0,1,-1};
 int y[4] = {1,-1,0,0};
@@ -11,7 +12,7 @@ int dm, dn;
 //vector 로 매개변수를 보낼 때 참조를 하지 않으면 메모리를 새롭게 만든다.
 //원래의 vector 배열에 지장을 주지않는 새로운 배열을 만들어서 연산하기 때문이다.
 //이렇게 참조를 하지 않으면 많은 메모리 접근으로 인해 프로그램이 느려진다. 
-int DFS(int i, int j, vector<vector<int>>& picture){
+int DFS(int i, int j,  vector<vector<int> > picture){
     check[i][j] = 1;
     int max = 1;
     
@@ -27,7 +28,7 @@ int DFS(int i, int j, vector<vector<int>>& picture){
     return max;
 }
 
-vector<int> solution(int m, int n, vector<vector<int>> picture) {
+vector<int> solution(int m, int n, vector<vector<int> > picture) {
     int number_of_area = 0;
     check.assign(m,vector<int>(n, 0));
     
@@ -45,5 +46,19 @@ vector<int> solution(int m, int n, vector<vector<int>> picture) {
         }
     }
     
+    for(int i = 0 ; i < m ; i++){
+    	for(int j = 0 ; j < n ; j++){
+    		cout << picture[i][j] << "	";
+		}
+		cout << endl;
+	}
+    
     return vector<int>{number_of_area, max};
+}
+
+int main (){
+	vector<int> a;
+	a = solution(13, 16, {{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0}, {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0}, {0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0}, {0, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 0}, {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0}, {0, 1, 3, 3, 3, 1, 1, 1, 1, 1, 1, 3, 3, 3, 1, 0}, {0, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 0}, {0, 0, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 0, 0}, {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0}});
+
+	cout << endl << a[0] << " : " << a[1] << endl;
 }
