dab1e5094d524f8c7d791611b73e3de208355305
inwooshin <60161818@mju.ac.kr>
2021-03-07 11:11:22 +0900

    p와 y의 개수
    문제 해설
    https://programmers.co.kr/learn/courses/30/lessons/12916
    
    해결
    p와 P 가 있을 경우에 answer 에 +1 을 해주고 y나 Y 가 있을 때 -1 을 해줘서 answer 가 0이면 True 아니면 false 를 리턴한다.

---
+++ python/문자열p와y개수.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = 0
+    
+    for i in s:
+        if i == 'p' or i == 'P':
+            answer += 1
+        elif i == 'y' or i == 'Y':
+            answer -= 1
+
+    if answer == 0:
+        return True
+    else :
+        return False
