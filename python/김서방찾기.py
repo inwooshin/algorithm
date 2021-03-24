3622a71d638ad4c6c50ac93c2cb69842292054f0
inwooshin <60161818@mju.ac.kr>
2021-03-24 11:55:35 +0900

    수정사항 오류 및 반영
     - 수정하지 않은 부분들이 있는데 다시 저장되면서 수정이 되었다고 나와서 커밋

--- python/김서방찾기.py
+++ python/김서방찾기.py
@@ -1,2 +1,3 @@
 def solution(seoul):
     return "김서방은 " + str(seoul.index("Kim")) + "에 있다"
+    
--- python/문자열to정수.py
+++ python/문자열to정수.py
@@ -2,16 +2,17 @@ def solution(s):
     answer = 0
     minus = 1
     count = len(s) - 1
+    on = 0
     
     if s[0] == '-':
         minus = -1
         count -= 1
+        on = 1
     elif s[0] == '+':
         count -= 1
+        on = 1
     
-    for i in s:
-        if i == '-': continue
-        elif i == '+': continue
+    for i in s[on:]:
         answer += (ord(i) - ord('0')) * minus * pow(10,count)
         count -= 1
         
