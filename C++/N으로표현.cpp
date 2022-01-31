624f58805558a0cf33b2b18ea192665ee84af449
inwooshin <dkrlehowl45@naver.com>
2022-01-31 16:20:21 +0900

    N으로 표현
    문제 설명
    https://programmers.co.kr/learn/courses/30/lessons/42895
    
    해결
    N 을 사용하는 횟수가 1일때의 경우는 N 하나만 쓰는 것이다. N을 2번 사용할때는 1번째의 N 을 통해서 사칙연산을 수행한 값이다. 3번째는 2번째와 1번째를 사칙연산을 수행한 값이다.
    1. 이처럼 N을 1번 쓸때, 사용한 값을 저장후에 해당배열 두개를 통해서 사칙연산을 수행하면된다.
    2. 위의 과정을 반복하면서 점진적으로 N을 증가시켜주면서 number 값이 등장하는지를 확인하고 등장하면 최소의 N 값을 리턴한다.

---
+++ C++/N으로표현.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int getNN(int N, int num){
+    int tmp = N;
+    
+    for(int i = 0 ; i < num ; i++){
+        tmp = tmp * 10 + N;
+    }
+    
+    return tmp;
+}
+
+int solution(int N, int number) {
+    if(N == number) return 1;
+    
+    int answer = 0;
+    vector<unordered_set<int>> set(8);
+    
+    set[0].insert(N);
+    
+    for(int i = 1 ; i < 8 ; i++){
+        for(int j = 0 ; j < i ; j++){
+            for(int k = 0 ; k < i ; k++){
+                if(k + j != i - 1) continue;
+                
+                for(int a : set[j]){
+                    for(int b : set[k]){
+                        set[i].insert(a + b);
+                        set[i].insert(a * b);
+                        if(a - b > 0)
+                            set[i].insert(a - b);
+                        if(a / b > 0)
+                            set[i].insert(a / b);
+                    }
+                }
+                
+            }
+        }
+        
+        set[i].insert(getNN(N, i));
+        if(set[i].count(number)) return i + 1;
+    }
+    
+    return -1;
+}
