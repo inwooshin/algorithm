98c0c82785ed127c9d50396f01137f865ddaebd3
inwooshin <dkrlehowl45@naver.com>
2022-04-25 22:58:44 +0900

    세금
    문제
    https://www.acmicpc.net/problem/20492
    
    해결
    1. 88%의 금액과 95.6% 의 금액을 출력한다.

---
+++ C++/세금.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, input;
+    
+    cin >> input;
+    
+    cout << (int)(input * 0.78) << " " << (int)(input * 0.956);
+
+    return 0;
+}
