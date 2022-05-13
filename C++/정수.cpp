f9ec3b2c26f276e9762c33a271c557e4033f3a7d
inwooshin <59462895+inwooshin@users.noreply.github.com>
2022-05-13 22:41:51 +0900

    정수 개수

---
+++ C++/정수.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int in, count = 0;
+    
+    while(cin >> in){
+        
+        if(in > 0){
+            count++;
+        }
+    }
+    
+    cout << count;
+    
+
+    return 0;
+}
