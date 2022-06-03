8c576c1e3e9fd87c8fd26762e53da517597b0e1d
inwooshin <59462895+inwooshin@users.noreply.github.com>
2022-06-03 14:20:43 +0900

    럭비클럽
    문제 설명
    https://www.acmicpc.net/problem/2083
    
    해결
    1. 나이가 일정 수준 넘고, 80키로 이상인 사람은 시니어로 출력하고 그 외는 주니어로 출력한다

---
+++ C++/럭비클럽.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age, weight;
+    
+    while(true){
+        cin >> name;
+        cin >> age >> weight;
+        
+        if(name == "#") break;
+        
+        if(age > 17 || weight >= 80)
+            cout << name << " Senior" << endl;
+        else
+            cout << name << " Junior" << endl;
+    }
+
+    return 0;
+}
